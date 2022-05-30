54fe0188cf0ed34a702aa7efa21fab64102cfa3c
Andy Hughes <andyh@emerye.com>
2022-05-30 05:55:17 -0700

    Update to use latest versions of tools

--- scripts/cdscheckSysConf.sh
+++ scripts/cdscheckSysConf.sh
@@ -4,28 +4,25 @@
 
 CDSDIR=/home/cadence/cadence/installs/
 
-#$CDSDIR/ICADVM201/tools/bin/checkSysConf ICADVM20.1
+$CDSDIR/ICADVM201_24/tools/bin/checkSysConf ICADVM20.1
 
-#$CDSDIR/PEGASUS213/tools/bin/checkSysConf PEGASUS21.3 
+$CDSDIR/PEGASUS213/tools/bin/checkSysConf PEGASUS21.3 
 
-#$CDSDIR/SPECTRE201/tools/bin/checkSysConf SPECTRE20.1
+$CDSDIR/SPECTRE211/tools/bin/checkSysConf SPECTRE21.1
 
-#$CDSDIR/QUANTUS201_000/tools/bin/checkSysConf QUANTUS20.1 
+$CDSDIR/QUANTUS212/tools/bin/checkSysConf QUANTUS21.2 
 
-#$CDSDIR/XCELIUM2009_022/tools/bin/checkSysConf XCELIUM2009 
+$CDSDIR/XCELIUM2109/tools/bin/checkSysConf XCELIUM2109 
 
 echo -n "ICADVM20.1 "
-$CDSDIR/ICADVM201/tools/bin/checkSysConf  ICADVM20.1 -q
+$CDSDIR/ICADVM201_24/tools/bin/checkSysConf  ICADVM20.1 -q
 echo -n "PEGASUS21.3 "
 $CDSDIR/PEGASUS213/tools/bin/checkSysConf  PEGASUS21.3 -q 
-echo -n "SPECTRE20.1 "
-$CDSDIR/SPECTRE201/tools/bin/checkSysConf  SPECTRE20.1 -q
-echo -n "QUANTUS20.1 "
-$CDSDIR/QUANTUS201_000/tools/bin/checkSysConf  QUANTUS20.1 -q 
-echo -n "XCELIUM2009 "
-$CDSDIR/XCELIUM2009_022/tools/bin/checkSysConf  XCELIUM2009 -q
-
-
-
+echo -n "SPECTRE21.1 "
+$CDSDIR/SPECTRE211/tools/bin/checkSysConf  SPECTRE21.1 -q
+echo -n "QUANTUS21.2 "
+$CDSDIR/QUANTUS212/tools/bin/checkSysConf  QUANTUS21.2 -q 
+echo -n "XCELIUM2109 "
+$CDSDIR/XCELIUM2109/tools/bin/checkSysConf  XCELIUM2109 -q
 
 
