15af29b00e83ff5731433a29522f434857a045b9
Andy Hughes <andyh@emerye.com>
2022-11-17 17:12:23 -0800

    ssh port number change

--- scripts/vpnlog.sh
+++ scripts/vpnlog.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm /home/andy/tmp/access.log
-rsync vpn://home/andy/scripts/access.log /home/andy/tmp
+rsync -e "ssh -p 2950" vpn://home/andy/scripts/access.log /home/andy/tmp
 mail -s "VPN Log" admin_notify@r2semi.com < /home/andy/tmp/access.log
