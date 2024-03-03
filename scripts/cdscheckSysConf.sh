8fc78946fb7d502568687561e63eae967220a19e
Andy Hughes <andyh@emerye.com>
2024-03-03 07:29:57 -0800

    RHSL8 updates

--- scripts/cdscheckSysConf.sh
+++ scripts/cdscheckSysConf.sh
@@ -4,14 +4,19 @@
 
 CDSDIR=/home/cadence/cadence/installs/
 
+echo "ICADVM20.1"
 $CDSDIR/ICADVM201_27/tools/bin/checkSysConf ICADVM20.1
 
+echo "ICADVM20.1 "
 $CDSDIR/PEGASUS221/tools/bin/checkSysConf PEGASUS22.1 
 
+echo "PEGASUS22.1 "
 $CDSDIR/SPECTRE211_484/tools/bin/checkSysConf SPECTRE21.1
 
+echo "QUANTUS21.2 "
 $CDSDIR/QUANTUS212/tools/bin/checkSysConf QUANTUS21.2 
 
+echo "XCELIUM2203 "
 $CDSDIR/XCELIUM2203/tools/bin/checkSysConf XCELIUM2203 
 
 echo -n "ICADVM20.1 "
