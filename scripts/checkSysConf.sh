4b4ef54e651da52cc9fdeccaac395dbe6ef01da1
Andy Hughes <andyh@emerye.com>
2024-03-06 20:13:21 -0800

    Add IC23.1

--- scripts/checkSysConf.sh
+++ scripts/checkSysConf.sh
@@ -3,11 +3,13 @@
 # RH8 checkSysConf version 3.53
 TOOLSDIR=/home/andy/tools.lna64/bin
 
+echo "IC231 "
+$TOOLSDIR/checkSysConf --noDISPLAY IC23.1
 
-echo "ICADVM20.1.201SP"
+echo "ICADVM20.1.201SP "
 $TOOLSDIR/checkSysConf --noDISPLAY ICADVM20.1.201SP
 
-echo "PEGASUS22.1"
+echo "PEGASUS22.1 "
 $TOOLSDIR/checkSysConf PEGASUS22.1 
 
 echo "SPECTRE21.1"
@@ -17,8 +19,13 @@ $TOOLSDIR/checkSysConf SPECTRE21.1
 #$TOOLSDIR/checkSysConf QUANTUS20.1 
 /home/cadence/cadence/installs/QUANTUS221/tools/bin/checkSysConf QUANTUS22.1
 
-echo "XCELIUM2103 "
-$TOOLSDIR/checkSysConf XCELIUM2103 
+echo -e "\n\nQuiet checks"
+
+echo -n "IC23.1 "
+$TOOLSDIR/checkSysConf IC23.1 --noDISPLAY -q
+
+echo -n "XCELIUM2103 "
+$TOOLSDIR/checkSysConf XCELIUM2103 -q
 
 echo -n "ICADVM20.1.201SP "
 $TOOLSDIR/checkSysConf --noDISPLAY ICADVM20.1.201SP -q 
